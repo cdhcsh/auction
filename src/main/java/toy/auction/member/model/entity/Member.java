2a6cae74d1a1bf32c64ebb50fe2e561a6b2f6506
cdhcsh <cdhcsh@gmail.com>
2023-08-07 23:42:48 +0900

    setting

---
+++ src/main/java/toy/auction/member/model/entity/Member.java
@@ -0,0 +1,11 @@
+package toy.auction.member.model.entity;
+
+public class Member {
+    private Long id;
+    private String memberId;
+    private String password;
+    private String email;
+    private long phoneNumber;
+    private String name;
+
+}
