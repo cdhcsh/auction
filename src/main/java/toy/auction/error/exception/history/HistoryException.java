23d22d6c7a869b70d99f35a5f64d859cd451576d
cdhcsh <cdhcsh@gmail.com>
2023-09-25 23:59:57 +0900

    setting

---
+++ src/main/java/toy/auction/error/exception/history/HistoryException.java
@@ -0,0 +1,9 @@
+package toy.auction.error.exception.history;
+
+import toy.auction.error.exception.AuctionException;
+
+public class HistoryException extends AuctionException {
+    public HistoryException(String str) {
+        super(str);
+    }
+}
