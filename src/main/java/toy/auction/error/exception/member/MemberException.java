cf668a12b2fa0d450fd8b9337bb8b2b60f5fbfba
cdhcsh <cdhcsh@gmail.com>
2023-09-27 00:03:16 +0900

    setting

---
+++ src/main/java/toy/auction/error/exception/member/MemberException.java
@@ -0,0 +1,9 @@
+package toy.auction.error.exception.member;
+
+import toy.auction.error.exception.AuctionException;
+
+public class MemberException extends AuctionException {
+    public MemberException(String str) {
+        super(str);
+    }
+}
