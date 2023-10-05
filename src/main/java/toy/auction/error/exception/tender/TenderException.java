235dc79cf22bd672916d013c6889a2788c05acb0
cdhcsh <cdhcsh@gmail.com>
2023-10-05 17:10:30 +0900

    setting

---
+++ src/main/java/toy/auction/error/exception/tender/TenderException.java
@@ -0,0 +1,9 @@
+package toy.auction.error.exception.tender;
+
+import toy.auction.error.exception.AuctionException;
+
+public class TenderException extends AuctionException {
+    public TenderException(String str) {
+        super(str);
+    }
+}
