12777edee4a8d300d21452563ef4d37964476a42
cdhcsh <cdhcsh@gmail.com>
2023-10-11 20:35:12 +0900

    setting

--- src/main/java/toy/auction/error/exception/item/ItemException.java
+++ src/main/java/toy/auction/error/exception/item/ItemException.java
@@ -3,6 +3,7 @@ package toy.auction.error.exception.item;
 import toy.auction.error.exception.AuctionException;
 
 public class ItemException extends AuctionException {
+    //item 예외 클래스
     public ItemException(String str){
         super(str);
     }
